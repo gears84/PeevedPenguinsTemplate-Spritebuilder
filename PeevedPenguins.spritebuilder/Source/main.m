a44bb06f30b2c1d4847e5b724688a8b4b95da8fb
Giuseppe <standard@Mac-mini-di-Giuseppe.local>
2014-10-24 17:27:12 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/main.m
+++ PeevedPenguins.spritebuilder/Source/main.m
@@ -8,10 +8,3 @@
 
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[]) {
-    
-    @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
-        return retVal;
-    }
-}
